0f5a6d0e95f0f972f9131bc388261f8040f0290a
festus <festuschiagozie169@gmail.com>
2024-09-14 21:17:12 +0100

    second commit

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleNav(){
     const navLinks = document.getElementById("navLinks");
     navLinks.classList.toggle("active");
 }
+const span = document.querySelectorAll('.progress-bar span');
+
+span.forEach((span) => {
+    span.style.width = span.dataset.width;
+    span.innerHTML = span.dataset.width;
+} )
